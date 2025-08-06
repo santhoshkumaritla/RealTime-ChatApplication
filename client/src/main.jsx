477da0ed995cb7cd546b8bed0d90d881cbbde8c8
santhoshkumaritla <itlasanthoshkumar@gmail.com>
2025-08-07 02:02:52 +0530

    fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/authContext';
+import { AuthProvider } from './context/authContext.jsx';
 import { ChatProvider } from './context/chatContext';
 
 createRoot(document.getElementById('root')).render(
